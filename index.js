c57a5c1f5558d0b3490d543655cf8e31a3b9c98f
YCL <121530406+YCL0609@users.noreply.github.com>
2024-02-17 21:31:59 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,164 @@
-function search() {
-    $(document).keydown(function (event) {
-        if (event.keyCode == 13) {
-            var word = document.getElementById('word').value;
-            if (word != "") {
-                var regular = /^([hH][tT]{2}[pP]:\/\/|[hH][tT]{2}[pP][sS]:\/\/)(([A-Za-z0-9-~]+)\.)+([A-Za-z0-9-~\/])+$/;
-                if (regular.test(word)) {
-                    window.open(word);
-                    location.reload();
-                } else {
-                    window.open("https://www.baidu.com/s?wd=" + word);
-                    location.reload();
-                };
-            }
+window.onload = () => {
+    const rootname = localStorage.getItem('rootname');
+    const input_Element = document.getElementById('root');
+    const select_Element = document.getElementById("RootSelect");
+    const markadd_Element = document.getElementById('markadd');
+    const markdel_Element = document.getElementById('markdel');
+    const markaddchick_Element = document.getElementById('markadd-chick');
+    const markdelchick_Element = document.getElementById('markdel-chick');
+
+    // 下拉选择菜单事件
+    select_Element.addEventListener("change", (event) => {
+        var selectedValue = event.target.value;
+        if (selectedValue == "self") { // 自定义
+            input_Element.style.display = "";
+            localStorage.setItem('rootname', 'self')
+        } else if (selectedValue == "baidu") { // 百度
+            localStorage.setItem('root', 'https://www.baidu.com/s?wd=');
+            localStorage.setItem('rootname', 'baidu');
+            input_Element.style.display = "none"
+        } else if (selectedValue == "bing") { // Bing
+            localStorage.setItem('root', 'https://cn.bing.com/search?q=');
+            localStorage.setItem('rootname', 'bing');
+            input_Element.style.display = "none"
         }
     });
+
+    // 自定义搜索引擎事件
+    input_Element.addEventListener('input', () => {
+        // 将自定义搜索引擎存储到localStone
+        localStorage.setItem('root', input_Element.value)
+    });
+
+    // 添加书签事件
+    markadd_Element.addEventListener('click', () => {
+        markadd_Element.style.display = "none";
+        markdel_Element.style.display = "none";
+        document.getElementById('markadd-div').style.display = "";
+    });
+
+    // 添加书签确认事件
+    markaddchick_Element.addEventListener('click', () => {
+        const title = document.getElementById('markadd-name').value;
+        const url = document.getElementById('markadd-url').value;
+        const id = Date.now(); // 创建一个唯一的ID用于标识书签
+        const bookmarkId = 'bookmark_' + id;
+        markadd_Element.style.display = "";
+        markdel_Element.style.display = "";
+        document.getElementById('markadd-div').style.display = "none";
+        if (title && url) {
+            // 创建一个包含URL和标题的对象
+            const bookmark = {
+                url: "http://" + url,
+                title: title,
+                id: id
+            };
+            // 将书签存储在localStorage中
+            localStorage.setItem(bookmarkId, JSON.stringify(bookmark));
+            // 显示书签
+            addbookmark("http://" + url, title, id);
+        }
+    })
+
+    // 删除书签事件
+    markdel_Element.addEventListener('click', () => {
+        markadd_Element.style.display = "none";
+        markdel_Element.style.display = "none";
+        document.getElementById('markdel-div').style.display = "";
+    });
+
+    // 删除书签确认事件
+    markdelchick_Element.addEventListener('click', () => {
+        markadd_Element.style.display = "";
+        markdel_Element.style.display = "";
+        document.getElementById('markdel-div').style.display = "none";
+        // 获取已选择选项的值和索引
+        var selectElement = document.getElementById('markdel-selsct');
+        var selectedIndex = selectElement.selectedIndex; // 获取当前选中项的索引
+        var selectedValue = selectElement.options[selectedIndex].value; // 获取当前选中项的文本内容
+        // 删除localStorage数据
+        localStorage.removeItem("bookmark_" + selectedValue);
+        // 删除下拉选择菜单数据
+        selectElement.remove(selectedIndex);
+        // 文字增加删除线
+        var linkElement = document.getElementById("bookmark-" + selectedValue)
+        linkElement.href = "#";
+        linkElement.style.textDecoration = "line-through";
+    })
+
+    // 设置用户默认搜索引擎
+    if (rootname == "baidu") { // 百度
+        var optionElement = select_Element.querySelector('option[value="baidu"]');
+        optionElement.selected = true
+    } else if (rootname == "self") { // 自定义
+        var optionElement = select_Element.querySelector('option[value="self"]');
+        optionElement.selected = true;
+        input_Element.value = localStorage.getItem('root')
+        input_Element.style.display = null
+    } else { // Bing或首次运行
+        localStorage.setItem('root', 'https://cn.bing.com/search?q=');
+        localStorage.setItem('rootname', 'bing');
+        var optionElement = select_Element.querySelector('option[value="bing"]');
+        optionElement.selected = true
+    };
+
+    // 书签处理
+    for (let i = 0; i < localStorage.length; i++) { // 遍历localStorage中的所有项
+        const key = localStorage.key(i);
+        // 检查是否是书签项
+        if (key.startsWith('bookmark_')) {
+            // 获取书签数据并解析为对象
+            const bookmark = JSON.parse(localStorage.getItem(key));
+            addbookmark(bookmark.url, bookmark.title, bookmark.id)
+        }
+    }
+
+    // 输入框回车事件
+    var word = document.getElementById('word')
+    word.addEventListener('keydown', e => {
+        if (e.key === 'Enter') {
+            urljump();
+            location.reload();
+        }
+    });
+}
+
+// 网页跳转事件
+function urljump() {
+    var url = document.getElementById("word").value;
+    if (url != "") {
+        // 使用正则表达式验证是否为URL
+        var pattern = new RegExp('^(https?:\\/\\/)' +// 协议部分
+            '((([a-zA-Z\\d]([a-zA-Z\\d-]{0,61}[a-zA-Z\\d])?)\\.)+[a-zA-Z]{2,}|' + // 域名部分
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // IP地址部分
+            '(\\:\\d+)?(\\/[-a-zA-Z\\d%_.~+]*)*' + // 端口号和路径部分
+            '(\\?[;&a-zA-Z\\d%_.~+=-]*)?' + // 查询字符串部分
+            '(\\#[-a-zA-Z\\d_]*)?$', 'i'); // 锚点部分
+        if (pattern.test(url)) {
+            window.open(url);
+        } else {
+            var root = localStorage.getItem('root');
+            window.open(root + url);
+        }
+    }
+}
+
+// 添加书签到网页
+function addbookmark(url, title, id) {
+    const bookDiv = document.getElementById('bookmark-index');
+    const delopt = document.getElementById('markdel-selsct');
+    // 创建元素并添加到书签div中
+    const link = document.createElement('a');
+    const br = document.createElement('br');
+    link.onclick = () => { window.open(url) };
+    link.textContent = title;
+    link.href = "#"
+    link.id = "bookmark-" + id;
+    bookDiv.appendChild(link);
+    bookDiv.appendChild(br);
+    // 创建元素并添加到删除书签中
+    const option = document.createElement('option');
+    option.value = id;
+    option.textContent = title;
+    delopt.appendChild(option);
 }
--- main.js
+++ main.js
@@ -1,69 +1,93 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, shell } = require('electron')
 
-const createWindow = () => {
+// 设置用户配置文件夹
+app.setPath('userData', process.cwd() + "\\resources\\Data");
+
+app.whenReady().then(() => {
   const win = new BrowserWindow({
     // 设置主窗口初始尺寸
     width: 1024,
     height: 600,
     // 设置主窗口最小尺寸
     minWidth: 1024,
-    minHeight: 600
+    minHeight: 600,
   })
   // 加载主页
-  win.loadFile('index.html')
-}
- 
-// 显示主窗口
-app.whenReady().then(() => {
-  createWindow( )
+  win.loadFile('index.html');
+
+  // 自定义菜单
+  const menuTemplate = [{
+    label: '编辑...',
+    submenu: [{
+      label: '剪切',
+      accelerator: 'Ctrl+X',
+      role: 'cut'
+    }, {
+      label: '复制',
+      accelerator: 'Ctrl+C',
+      role: 'copy'
+    }, {
+      label: '粘贴',
+      accelerator: 'Ctrl+V',
+      role: 'paste'
+    }, {
+      label: '粘贴为纯文本',
+      accelerator: 'CmdOrCtrl+Shift+V',
+      role: 'pasteAndMatchStyle'
+    }, {
+      type: 'separator'
+    }, {
+      label: '撤销',
+      accelerator: 'Ctrl+Z',
+      role: 'undo'
+    }, {
+      label: '全选',
+      accelerator: 'Ctrl+A',
+      role: 'selectall'
+    },]
+  }, {
+    label: '刷新 (F5)',
+    accelerator: 'F5',
+    role: 'reload'
+  }, {
+    label: '忽略缓存刷新 (shift+F5)',
+    accelerator: 'shift+F5',
+    role: 'forceReload'
+  }, {
+    label: '全屏 (F11)',
+    accelerator: 'F11',
+    role: 'toggleFullScreen'
+  }, {
+    label: '开发者工具 (F12)',
+    accelerator: 'F12',
+    role: 'toggleDevTools'
+  }, {
+    label: 'Print Current URL',
+    click: () => {
+      const currentWindow = BrowserWindow.getFocusedWindow();
+      win.webContents.executeJavaScript(`
+      localStorage.setItem('page','`+ currentWindow.webContents.getURL() +`')`);
+      const newwin = new BrowserWindow({
+        // 设置主窗口初始尺寸
+        width: 500,
+        height: 200,
+      })
+      // 加载主页
+      newwin.setMenu(null)
+      newwin.loadFile('url.html');
+    }
+  }, {
+    label: '关闭菜单(全局)',
+    click: () => {
+      Menu.setApplicationMenu(null);
+    }
+  }
+  ]
+
+  const menuBuilder = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menuBuilder)
 })
 // 当所有的窗口被关闭后退出应用
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') { app.quit() }
 })
-
-// 自定义菜单
-const menuTemplate = [{
-  label: '编辑...',
-  submenu: [{
-    label: '剪切',
-    accelerator: 'Ctrl+X',
-    role: 'cut'
-  }, {
-    label: '复制',
-    accelerator: 'Ctrl+C',
-    role: 'copy'
-  }, {
-    label: '粘贴',
-    accelerator: 'Ctrl+V',
-    role: 'paste'
-  }, {
-    type: 'separator'
-  }, {
-    label: '撤销',
-    accelerator: 'Ctrl+Z',
-    role: 'undo'
-  }, {
-    label: '全选',
-    accelerator: 'Ctrl+A',
-    role: 'selectall'
-  },]
-}, {
-  label: '刷新 (F5)',
-  accelerator: 'F5',
-  role: 'reload'
-}, {
-  label: '忽略缓存刷新 (shift+F5)',
-  accelerator: 'shift+F5',
-  role: 'forceReload'
-}, {
-  label: '全屏 (F11)',
-  accelerator: 'F11',
-  role: 'toggleFullScreen'
-}, {
-  label: '开发者工具 (F12)',
-  accelerator: 'F12',
-  role: 'toggleDevTools'
-}]
-const menuBuilder = Menu.buildFromTemplate(menuTemplate);
-Menu.setApplicationMenu(menuBuilder)
